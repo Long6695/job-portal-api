d21bf9dc1f35b759c90a3c597aacff6ce4c977c5
ryu <thaihothanhlong95@gmail.com>
2022-11-29 14:30:19 +0700

    chore: init

---
+++ prisma/seed.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+export const prisma = new PrismaClient();
+
+(async function main() {
+    try {
+      const user = await prisma.user.create({
+        data: {
+            name: 'admin',
+            email: 'admin@jobportal.com',
+            password: '123123',
+            passwordConfirm: '123123',
+            verify: true,
+            role: 'ADMIN',
+            profile: {
+                create : {
+                    firstName : 'admin',
+                    lastName  :  'admin',
+                    bio       : 'Hi i am Long',
+                    birthDate : '1995-11-28T14:34:59.708Z',
+                }
+            }
+        }
+      }
+      );
+      console.log('Create 1 user: ', user);
+    } catch(e) {
+      await prisma.$disconnect();
+      console.error(e);
+      process.exit(1);
+    } finally {
+      await prisma.$disconnect();
+    }
+  })();
